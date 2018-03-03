f3ad2ed6c8fe00e6c04a788b2f7fee130d679141
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-03 22:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var name = "Susan"
 var height = "70"
 
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
+
+
 if (index.height === 70) {
     console.log("The height is correct")
+} else {
+    console.log("Expected: 70")
 }
 
 // Don't worry about this
