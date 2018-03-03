b6caed3e9505218b6774f756e1cb0c006a846670
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-03 22:51:08 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,7 +2,9 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} 
+} else {
+    console.log("Expected the name to be Susan")
+}
 
 
 if (index.height === 70) {
