509b0db7bdd0227cd46fee59e1855acd0352a4e1
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-03 22:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var name = "Susan"
 var height = "70"
 
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} 
+
+
+if (index.height === 70) {
+    console.log("The height is correct")
+
 // Don't worry about this
 module.exports = { name, height
 }
